f2761a75940b6e970b9746dd4b88205dbf5e1cf8
jason.chang <jasonchang.ccs@gmail.com>
2020-10-21 16:19:33 +0800

    commit new

---
+++ Python Basic/pr1.py
@@ -0,0 +1,12 @@
+# This is first app to show Python Syntax.
+print('Hello, This is python.')
+print('What is your name?')
+yourName = input()
+print('Hello, ' + yourName + '~')
+print('Nice to meet you~')
+"""
+This is multi-line comment.
+1
+2
+3
+"""
