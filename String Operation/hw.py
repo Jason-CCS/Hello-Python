2bc5b1b969da96f814f5fb4e743323cd51ff2283
jason.chang <jasonchang.ccs@gmail.com>
2020-12-08 16:21:41 +0800

    commit new content

--- String Operation/hw.py
+++ String Operation/hw.py
@@ -4,6 +4,7 @@ step = 4
 layer = 5
 level = 4
 
+# print leaves
 for i in range(level):
     startNumber = i * step + 1
     endNumber = startNumber + layer * step
@@ -11,6 +12,7 @@ for i in range(level):
         stars = '*' * j
         print(stars.center(100, ' '))
 
+# print trunk
 for k in range(5):
     stars = '*' * 5
     print(stars.center(100, ' '))
