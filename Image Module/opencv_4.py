17f1867e42a47e75ba1ea86e153f04c6f122b608
jason.chang <jasonchang.ccs@gmail.com>
2021-01-13 16:13:11 +0800

    commit new content

--- Image Module/opencv_4.py
+++ Image Module/opencv_4.py
@@ -1,19 +1,24 @@
 """
 使用opencv操作影像
 
-三、圖像操作
+四、圖像操作
 原文網址：https://kknews.cc/code/gg2bej8.html
 """
 import cv2
 
 img = cv2.imread('lena.png', cv2.IMREAD_COLOR)
 
-"""
-填滿ROI
-"""
-face = img[180:390, 220:370]
-img[180:390, 220:370] = [255, 255, 255]
-cv2.imshow('lena_face', img)
+px = img[180, 220] # get pixel
+print(px)
+
+face = img[180:390, 220:370] # get ROI
+print(face.shape)
+# print(face.size)
+# print(face.dtype)
+
+cv2.imshow('lena_face', face)
+img[180:390, 220:370] = [255, 255, 255] # 改變某區域像素
+cv2.imshow('lena_filled', img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ Image Module/opencv_6.py
@@ -0,0 +1,6 @@
+"""
+使用opencv操作影像
+
+六、閾值
+原文網址：https://kknews.cc/code/gg2bej8.html
+"""
