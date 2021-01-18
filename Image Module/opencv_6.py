4a8a4b11499e79a696cb00ae505c5c6e3d7deffd
jason.chang <jasonchang.ccs@gmail.com>
2021-01-18 16:28:55 +0800

    commit new content

--- Image Module/opencv_6.py
+++ Image Module/opencv_6.py
@@ -8,11 +8,19 @@ import cv2
 import numpy as np
 
 img = cv2.imread('bookpage.jpg')
-retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
-cv2.imshow('1', img)
-cv2.imshow('threshold', threshold)
+grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+"""
+自適應閥值法
+"""
+threshold = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 1)
+cv2.imshow('original', img)
+cv2.imshow('adaptive threshold', threshold)
+
+"""
+大津閥值法
+"""
+# retval2, threshold2 = cv2.threshold(grayscaled, 125, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# cv2.imshow('Otsu threshold', threshold2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
