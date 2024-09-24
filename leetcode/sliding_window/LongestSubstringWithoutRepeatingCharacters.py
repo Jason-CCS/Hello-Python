aa335a4564bbb92dc58f25300603da4d0ff75472
Jason Chang <jasonchang.ccs@gmail.com>
2024-09-24 23:38:05 +0800

    Add LongestSubstringWithoutRepeatingCharacters solution
    Implemented the sliding window technique to solve the "Longest Substring Without Repeating Characters" problem. The solution efficiently tracks characters and their positions, adjusting the window's start position to maintain the longest possible substring without duplicates. Added a main function to test the implementation with a sample input.

--- leetcode/sliding_window/LongestSubstringWithoutRepeatingCharacters.py
+++ leetcode/sliding_window/LongestSubstringWithoutRepeatingCharacters.py
@@ -1,5 +1,21 @@
 class Solution:
+    """
+    Problem: 3
+    Difficulty: Medium
+    """
+
     def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Using sliding window approach.
+        window:[start, end].
+        We iterate through the string by index end, and move the start when we find the same character in map table.
+        Sometimes we will find that the index get from map table is smaller than current start index.
+        When this happens, we will use line 21, max(start, d[s[end]]+1) to keep the start idx at the original position.
+        Time: O(n) as we only iterate the end idx from 0~n-1.
+        Space: O(distinct characters) as we use dictionary to store the characters.
+        :param s:
+        :return:
+        """
         max_length, d, start, end = 0, {}, 0, 0
 
         while end < len(s):
