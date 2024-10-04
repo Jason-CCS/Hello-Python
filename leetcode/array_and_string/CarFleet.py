e299c155c52a22f55c85daca40e4a0c4913ad0ae
Jason Chang <jasonchang.ccs@gmail.com>
2024-10-04 23:33:28 +0800

    Add CarFleet class implementation
    Introduced the CarFleet class with an initial method for calculating the number of car fleets. The main part demonstrates sorting cars by their starting positions and forming groups based on their arrival times at the target.

--- leetcode/array_and_string/CarFleet.py
+++ leetcode/array_and_string/CarFleet.py
@@ -1,20 +1,31 @@
 class CarFleet:
+
     def solution(self, position, speed, target):
-        zip(position, speed)
-        # sorted_zip_ary = zip_ary
+        """
+        This approach is from Neetcode
+        Sort the car array by position in descending order.
+        By counting the time from pos to target,
+        we can compare the time in descending order and understand if the current time spending is greater than cur_max,
+        we will introduce one more car group.
+        For other cases, they will be the same group because the speed of the car behind any latter car is faster than
+        the latter car, and they will be the same group.
+        Time Complexity: O(n*log(n)) cause sort.
+        :param position:
+        :param speed:
+        :param target:
+        :return:
+        """
+        car_list = sorted(zip(position, speed), reverse=True)
+        group, cur_max = 0, float('-inf')
+        for pos, sp in car_list:
+            time = (target - pos) / sp
+            if time > cur_max:
+                group += 1
+            cur_max = max(cur_max, time)
+
+        return group
 
 
 if __name__ == '__main__':
     position, speed, target = [10, 8, 0, 5, 3], [2, 4, 1, 1, 3], 12
-    n = len(position)
-    car_list = sorted(zip(position, speed), reverse=True)
-    # print(car_list)
-    cur_max = -1
-    group = 0
-    for pos, sp in car_list:
-        time = (target - pos) / sp
-        if time > cur_max:
-            group += 1
-        cur_max = max(cur_max, time)
-
-    print(group)
+    print(CarFleet().solution(position, speed, target))
